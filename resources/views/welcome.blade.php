5b5ef952e45ec321521b7d7c0840eaa249ab2465
tienntps09110 <natriwit675111@gmail.com>
2020-05-24 23:48:17 +0700

    add class subject, route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,11 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        .text-center {
+            text-shadow: 0 0 2px #fff;
+        }
+    </style>
   </head>
   <body>
       <div class="alert alert-success" role="alert">
@@ -17,14 +22,14 @@
           </div>
       <div class="container col-6">
           {{-- AUTH --}}
-          <div class="alert alert-primary text-center font-weight-bold">AUTH</div>
+          <div id="auth" class="alert text-center font-weight-bold">AUTH</div>
           @foreach($auth as $au)
           {{ $au['name'] }} ({{ $au['method'] }}): <a href="{{ route($au['route']) }}">CLICK</a>
           <br>
           @endforeach
           <br>
           {{-- USERS --}}
-          <div class="alert alert-danger text-center font-weight-bold">USERS</div>
+          <div id="users" class="alert text-center font-weight-bold">USERS</div>
           @foreach($users as $ad)
             @if($ad['route'] == 'get-user')
                 {{ $ad['name'] }} ({{ $ad['method'] }}): <a href="{{ route($ad['route'], '053dca80-9178-4551-b0f1-2f30a66cad92') }}">CLICK</a>
@@ -35,7 +40,7 @@
           @endforeach
           <br>
           {{-- CLASS --}}
-          <div class="alert alert-dark text-center font-weight-bold">CLASS</div>
+          <div id="class" class="alert text-center font-weight-bold">CLASS</div>
           @foreach($classM as $class)
             @if($class['route'] == 'get-class-detail' || $class['route'] == 'update-class-view')
                 {{ $class['name'] }} ({{ $class['method'] }}): <a href="{{ route($class['route'], '1') }}">CLICK</a>
@@ -46,7 +51,7 @@
           @endforeach
           <br>
           {{-- STUDENT --}}
-          <div class="alert alert-dark text-center font-weight-bold">STUDENT</div>
+          <div id="student" class="alert  text-center font-weight-bold">STUDENT</div>
           @foreach($students as $student)
             @if($student['route'] == 'get-student' || $student['route'] == 'update-student')
                 {{ $student['name'] }} ({{ $student['method'] }}): <a href="{{ route($student['route'], '76') }}">CLICK</a>
@@ -57,7 +62,7 @@
           @endforeach
           <br>
           {{-- SUBJECT --}}
-          <div class="alert alert-dark text-center font-weight-bold">SUBJECT</div>
+          <div id="subject" class="alert  text-center font-weight-bold">SUBJECT</div>
           @foreach($subjects as $subject)
             @if($subject['route'] == 'get-subject' || $subject['route'] == 'update-subject-view')
                 {{ $subject['name'] }} ({{ $subject['method'] }}): <a href="{{ route($subject['route'], '1') }}">CLICK</a>
@@ -68,7 +73,7 @@
           @endforeach
           <br>
           {{-- CLASS SUBJECT --}}
-          <div class="alert alert-dark text-center font-weight-bold">CLASS SUBJECT</div>
+          <div id="classSubject" class="alert  text-center font-weight-bold">CLASS SUBJECT</div>
           @foreach($classSubjects as $cs)
             @if($cs['route'] == 'get-subject' || $cs['route'] == 'update-subject-view')
                 {{ $cs['name'] }} ({{ $cs['method'] }}): <a href="{{ route($cs['route'], '1') }}">CLICK</a>
@@ -83,5 +88,22 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script>
+        function getRandomColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+        $('#auth').css('background-color', getRandomColor());
+        $('#users').css('background-color', getRandomColor());
+        $('#class').css('background-color', getRandomColor());
+        $('#student').css('background-color', getRandomColor());
+        $('#subject').css('background-color', getRandomColor());
+        $('#classSubject').css('background-color', getRandomColor());
+
+    </script>
   </body>
 </html>
