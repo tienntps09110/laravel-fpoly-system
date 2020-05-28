a768bc5fa0bec7c43820bc07e37e2934644045f0
neo_binh <neo.nguyen88@gmail.com>
2020-05-28 07:53:55 +0700

    update phan lop

--- resources/views/department/create-class-subject.blade.php
+++ resources/views/department/create-class-subject.blade.php
@@ -50,8 +50,108 @@
         <div class="container-fluid py-3">
             <div class="box cong-cu-phan-lop">
                 <h3 class="text-center">Công cụ Phân lớp</h3>
-                <div class="table-responsive">
-                    <table class="table mt-5"  >
+                <div class="">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="container-fluid my-4">
+                                <label for="lop"> Lớp 
+                                    <select name="class_id" id="lop" class="form-control"> 
+                                            @foreach ($class as $classDetail)
+                                                <option value="{{ $classDetail->id }}">{{ $classDetail->name }}</option>
+                                            @endforeach
+                                    </select>
+                                </label> 
+                            </div>
+                            <div class="container-fluid ">
+                                <label for="thu-hoc"> Chọn Các Thứ học trong tuần   
+                                    <select name="days_study[]" id="thu-hoc" class="form-control" >
+                                        <option value="">Chọn thứ </option>
+                                        <option value="0">Chủ nhật</option>
+                                        <option value="1">Thứ hai</option>
+                                        <option value="2">Thứ ba</option>
+                                        <option value="3">Thứ tư</option>
+                                        <option value="4">Thứ năm</option>
+                                        <option value="5">Thứ sáu</option>
+                                        <option value="6">Thứ bảy</option>
+                                    </select>   
+                                </label>
+                                <script>
+                                    $(document).ready(function(){
+                                        $("#thu-hoc").change(function(){
+                                            var t = $(this).val();
+                                            var show_t = $("#thu-hoc").children("option[value='"+t+"']").text();
+                                            
+                                            $('#result-weekday').append(`<div class="badge badge-pill badge-primary" style="padding:0.3em 0.5em;font-weight:normal">
+                                                                            <span class='small'>${show_t}</span> 
+                                                                            <span class='close-badge' id='${t}'><i class="fas fa-times" style="cursor:pointer;transform:scale(0.8)" ></i></span>
+                                                                            <input class='' id='' name='days_study[]' value='${t}' hidden>  
+                                                                        </div>
+                                                                        `);
+                                            $("#thu-hoc").val("");
+                                            $("#thu-hoc").children("option[value='"+t+"']").hide();
+                                            $(".close-badge").click(function(){
+                                                $(this).parent().remove();
+                                                var unhide_target = $(this).attr("id");
+                                                $("#thu-hoc").children("option[value="+unhide_target+"]").show();
+                                            })
+                                        })
+                                    })    
+                                </script>
+                            </div>
+                            <div class="container-fluid">
+                                <div id='result-weekday'></div>
+                            </div>
+
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="container-fluid my-4">
+                                <label for="mon"> Môn
+                                    <select name="subject_id" id="mon" class="form-control">
+                                        <option selected disabled>Chọn môn</option>
+                                            @foreach ($subjects as $subject)
+                                                <option value="{{ $subject->id }}">{{ $subject->name }}</option>
+                                            @endforeach
+                                    </select>
+                                </label>
+                            </div>       
+                            <div class="container-fluid my-4">
+                                <label for="from-date">Từ ngày
+                                    <input type="date" name="datetime_start" id="from-date" class="form-control">
+                                </label>
+                            </div>
+                            <div class="container-fluid my-4">
+                                <h5>Giáo viên Phụ trách</h5>
+                                <div class="form-inline">
+                                    <button id="chon-giao-vien" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                                        Chon
+                                    </button>                                    <div></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4">
+                            <div class="container-fluid my-4">
+                                <label for="ca"> Ca
+                                    <select name="study_time_id" id="ca" class="form-control">
+                                        @foreach ($studyTime as $studyTimeDetail)
+                                            <option value="{{ $studyTimeDetail->id }}">{{ $studyTimeDetail->name }} ({{ $studyTimeDetail->time_start . ' - ' .$studyTimeDetail->time_end }})</option>
+                                        @endforeach
+                                    </select>
+                                </label>
+                                    
+                            </div>
+                            <div class="container-fluid my-4">
+                                <label for="to-date">Đến ngày
+                                    <input type="date" name="datetime_end" id="to-date" class="form-control">
+                                </label>
+                            </div>
+                            <div class="container-fluid my-4">
+                                <button type="submit" class="btn  btn-primary mt-4">Xác nhận</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- <table class="table mt-5"  >
                         <tr>
                             <td>
                                 <label for="lop"> Lớp 
@@ -95,7 +195,7 @@
                                         <option value="5">Thứ sáu</option>
                                         <option value="6">Thứ bảy</option>
                                     </select>   
-                                    {{-- <input id="them-thu" type="button" value="+ Add" class="btn btn-dark">                                   --}}
+                                    {{-- <input id="them-thu" type="button" value="+ Add" class="btn btn-dark">                                --}}
                                 </label>
                                
                                 <script>
@@ -103,17 +203,20 @@
                                         $("#thu-hoc").change(function(){
                                             var t = $(this).val();
                                             var show_t = $("#thu-hoc").children("option[value='"+t+"']").text();
-                                       
-                                            $('#result-weekday').append(`<div class="badge badge-pill badge-primary ">
-                                                                            <span class='small '>${show_t}</span> 
-                                                                            <span class='close-badge'><i class="fas fa-times" style="cursor:pointer;transform:scale(0.8)" ></i></span>
-                                                                        </div><br>
+                                            
+                                            $('#result-weekday').append(`<tr><td class="badge badge-pill badge-primary" style="padding:0.3em">
+                                                                            <span class='small'>${show_t}</span> 
+                                                                            <span class='close-badge' id='${t}'><i class="fas fa-times" style="cursor:pointer;transform:scale(0.8)" ></i></span>
+                                                                        </td></tr>
                                                                         `);
-                                            $(".close-badge").click(function(){
-                                                console.log("abc")
-                                            })
-                                            $("#thu-hoc").children("option[value='']").hide();
+                                            // $("#thu-hoc").children("option[value='']").hide();
+                                            $("#thu-hoc").val("");
                                             $("#thu-hoc").children("option[value='"+t+"']").hide();
+                                            $(".close-badge").click(function(){
+                                                $(this).parent().add($(this).parent().next()).remove();
+                                                var unhide_target = $(this).attr("id");
+                                                $("#thu-hoc").children("option[value="+unhide_target+"]").show();
+                                            })
                                         })
                                         
                                     })    
@@ -145,7 +248,7 @@
                                 <button type="submit" class="btn  btn-primary mt-4">Xác nhận</button>
                             </td>
                         </tr>
-                    </table>
+                    </table> -->
                 </div>
             </div>
         </div>
